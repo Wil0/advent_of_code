9627b94e2581997a66284bfa4c268e35a68173fd
Emilio Menéndez <emsmnndz@gmail.com>
2019-02-11 19:33:54 +0000

    Day2 Part2

--- day2/part2/identical.rb
+++ day2/part2/identical.rb
@@ -1,21 +1,21 @@
 class Identical
-  def sef.calculate
-    arr = ['fghij','klmno','pqrst','fguij','axcye','wvxyz']
+  LINES = File.read('./day2/id_list.txt')
 
-    # input = File.read('../day2/id_list.txt')
+  def self.run
+    LINES.each_line do |line_a|
+      LINES.each_line do |line_b|
+        diff_count = 0
+        common_letters = ''
 
-    i = 1
-    while i < arr.size
-      arr.each do |line|
-        @current_line = line.delete("\n").to_s.split('')
-        @next_line = arr[i].delete("\n").to_s.split('')
-        binding.pry
-        break if (@current_line - @next_line).count == 1
+        line_a.each_char.with_index do |char_a, index|
+          char_b = line_b[index]
+          char_a == char_b ? common_letters << char_a : diff_count += 1
+        end
 
-        i += 1
+        return common_letters if diff_count == 1
       end
     end
-
-    [@next_line, @current_line]
   end
 end
+
+Identical.run
